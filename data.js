891c0e39159ba37ea91f66c189c5514a2008b9fe
Ankush Saggu <ankush22004@gmail.com>
2025-06-27 16:18:36 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const reels = [
+  {
+    title: "Peacock Dance",
+    url: "videos/reel1.mp4",
+    likes: 21,
+    comments: ["Wow!", "Nice!"]
+  },
+  {
+    title: "Bharat Ki Jhalak",
+    url: "videos/reel2.mp4",
+    likes: 45,
+    comments: []
+  }
+];
